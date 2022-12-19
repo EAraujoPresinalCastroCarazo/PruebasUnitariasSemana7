25f435001d34a9fa175f4a831adaf307b4e4b86f
EAraujoPresinalCastroCarazo <estefanyaraujo835@hotmail.com>
2022-12-19 08:09:07 -0600

    Otros Cambios

--- PruebasUnitariasSemana7/clsCalculadora.cs
+++ PruebasUnitariasSemana7/clsCalculadora.cs
@@ -8,24 +8,20 @@ namespace PruebasUnitariasSemana7
     {
         public static int Suma( int a, int b)
         {
-
             return a + b;
         }
         public static int Resta(int a, int b)
         {
-
             return a - b;
         }
     
         public static int Multiplicacion(int a, int b)
         {
-
             return a * b;
         }
 
         public static double Division (int a, int b)
         {
-
             return a / b;
         }
     }
