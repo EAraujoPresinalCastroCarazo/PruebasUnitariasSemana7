375896830209f658c622c3327b6d467752059f8a
Estefany Araujo Presinal <earaujo@datasoftla.com>
2022-12-18 21:25:15 -0600

    Agregar archivos de proyecto.

---
+++ PUsem7Test/clsCalculadoraTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PUsem7Test
+{
+    class clsCalculadoraTest
+    {
+    }
+}
---
+++ PruebasUnitariasSemana7/clsCalculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebasUnitariasSemana7
+{
+   public class clsCalculadora
+    {
+        public static int Suma( int a, int b)
+        {
+
+            return a + b;
+        }
+        public static int Resta(int a, int b)
+        {
+
+            return a - b;
+        }
+    
+        public static int Multiplicacion(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public static double Division (int a, int b)
+        {
+
+            return a / b;
+        }
+    }
+}
